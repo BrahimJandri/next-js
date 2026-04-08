827930a89a998ee5c7ea47573dbbbd0787ba58bc
BrahimJandri <bzaza066@gmail.com>
2026-04-08 18:46:25 +0100

    not-found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,7 @@
+import {bgBlue, green, red, white} from "next/dist/lib/picocolors";
+
 export default function notFound() {
     return (
-        <div>Not Found</div>
+        <div className="mini-h-screen  flex items-center bg-blue-900 text-red">Not Found</div>
     );
 }
